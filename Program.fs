80b2651d61f14453584c7ee1f961574957f20b32
Ilya <whitesteckeraaa@gmail.com>
2023-02-26 11:39:13 +0300

    quicksort

--- Program.fs
+++ Program.fs
@@ -15,10 +15,30 @@ module PigLatin =
         else
             word[1..] + string(word[0]) + "ay"
 
+
+let rec quicksort list = 
+    match list with
+    | [] ->
+        []
+    | firstElem::otherElements ->
+        let smallerElements = 
+            otherElements
+            |> List.filter(fun e->e < firstElem)
+            |> quicksort
+        let largerElements =
+            otherElements
+            |> List.filter(fun e ->e >= firstElem)
+            |> quicksort
+
+        List.concat [smallerElements; [firstElem]; largerElements]
+
+
+
 [<EntryPoint>]
 let main args =
     for arg in args do
         let newArg = PigLatin.toPigLatin arg
         printfn "%s in Pig Latin is: %s" arg newArg
+        printf "%A" (quicksort [1;5;23;18;9;1;3])
 
     0
